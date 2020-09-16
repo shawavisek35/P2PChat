3297871f332162cd637294c28c744d6642e8bb20
shawavisek35 <shawavisek35@gmail.com>
2020-09-16 21:05:36 +0530

    initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container main-container mt-5 shadow-lg">
+      <div className="header-section">
+        <p className="px-2 py-2">P2Pchat</p>
+      </div>
     </div>
   );
 }
