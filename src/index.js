c2e60d1434f072dcce51c34c84678a788a330b5f
shawavisek35 <shawavisek35@gmail.com>
2020-09-19 19:55:27 +0530

    certain packages added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'jquery';
+import 'popper.js';
+import "bootstrap/dist/js/bootstrap.bundle";
 
 ReactDOM.render(
   <React.StrictMode>
